c0588e8933c4351582a41c985313a584fd0cb794
Paul Dowdall <r39ij6ua@students.codeinstitute.net>
2024-12-07 16:14:59 +0000

    Function create_baord to create empty game board

--- run.py
+++ run.py
@@ -1,1 +1,6 @@
+import random 
 
+#Function to create an empty game board
+def create_board(size=5):
+    #Create a size x size grid initialized with "~"
+    return [["~" for _ in range(size)] for _ in rnage(size)]
